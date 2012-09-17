0981c1a88a7e07be7560d88967c70ec14020d4a9
Russell Smith <russell.smith@ukd1.co.uk>
2012-09-16 23:53:16 -0700

    Fix port shit

--- app.py
+++ app.py
@@ -60,5 +60,6 @@ def current():
     return json.dumps(sonos.get_current_track_info())
 
 if __name__ == "__main__":
-    app.run()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
 
